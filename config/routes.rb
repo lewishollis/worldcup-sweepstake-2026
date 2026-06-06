4668d4b8182b5ad3b3b8a4f9806231b3f1f5a9aa
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2026-06-06 19:06:14 +0100

    chore: remove update_team_progress route

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  # Define routes for MatchesController
   resources :matches, only: [:index, :show]
 
-  # Define routes for LeaderboardController
   resources :leaderboard, only: [:index, :show] do
-    patch :update_team_progress, on: :member
     get :team, on: :collection
   end
 
-  # Game routes
   get  '/game',        to: 'games#index'
   post '/game/scores', to: 'games#create'
   get  '/game/scores', to: 'games#scores'
 
-  # Set root route
   root 'matches#index'
 end

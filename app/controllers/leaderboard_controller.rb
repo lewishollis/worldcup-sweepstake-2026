1c7d7178ad9c6e4857f9cc1ed8cbbdb26f3a2b13
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2026-06-06 19:01:57 +0100

    feat: LeaderboardController eager-loads matches for N+1 free scoring, removes manual toggle

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,8 +1,8 @@
 class LeaderboardController < ApplicationController
-  before_action :require_admin, only: [:update_team_progress]
-
   def index
-    @groups = Group.includes(:teams, :friend).all.sort_by { |group| -group_total_points(group) }
+    @groups = Group.includes(:friend, teams: [:home_matches, :away_matches])
+                   .all
+                   .sort_by { |group| -group.total_points }
     ctx = TournamentContextService.new
     @pivotal_matches = ctx.pivotal_matches(count: 3)
     @pivotal_scenarios = @pivotal_matches.each_with_object({}) do |match, h|
@@ -12,33 +12,18 @@ class LeaderboardController < ApplicationController
 
   def show
     if params[:id] =~ /\A\d+\z/
-      @group = Group.includes(:teams).find(params[:id])
+      @group = Group.includes(teams: [:home_matches, :away_matches]).find(params[:id])
       render :group_detail
     else
-      @leaderboard = Group.includes(:friend).sort_by { |group| -group.total_points }
+      @leaderboard = Group.includes(:friend, teams: [:home_matches, :away_matches])
+                          .sort_by { |group| -group.total_points }
       @ben_botcurdy_insight = BenBotcurdyService.new(:leaderboard).generate_insight
     end
   end
 
-
-
   def team
     @team = Team.find(params[:team_id])
     @matches = @team.matches.order(:start_time)
-  end
-
-  def update_team_progress
-    team = Team.find(params[:id])
-    was_progressed = team.progressed?
-    new_progressed = !was_progressed
-
-    if new_progressed && !was_progressed
-      team.update!(progressed: true, points: (team.points || 0) + 1)
-    elsif !new_progressed && was_progressed
-      team.update!(progressed: false, points: [(team.points || 0) - 1, 0].max)
-    end
-
-    redirect_to leaderboard_index_path
   end
 
   private
@@ -48,9 +33,5 @@ class LeaderboardController < ApplicationController
     authenticate_or_request_with_http_basic("Admin") do |_username, password|
       ActiveSupport::SecurityUtils.secure_compare(password, admin_password)
     end
-  end
-
-  def group_total_points(group)
-    group.teams.sum(&:points).to_i
   end
 end
